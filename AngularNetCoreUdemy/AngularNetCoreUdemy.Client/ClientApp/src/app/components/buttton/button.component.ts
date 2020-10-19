6d735928004736e0e5c3d7653a225ba4d0a8a7d5
Jahupe <69616693+Jahupe@users.noreply.github.com>
2020-10-18 20:44:25 -0500

    Load init

---
+++ AngularNetCoreUdemy/AngularNetCoreUdemy.Client/ClientApp/src/app/components/buttton/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:"button-agregar",
+  template:"<button class='btn btn-primary'>Agregar</button>"
+})
+
+export class ButonAgregar {
+  
+}
+
+ 
